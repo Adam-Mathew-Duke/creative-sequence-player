9e9c1c0aaff44b7d40cfe9605637640c721a891e
Adam Duke <0soy.fish.studios0@gmail.com>
2026-07-18 22:19:31 +1000

    Reorganize imports and maintain TrackLoader functionality

--- JS/trackLoader.js
+++ JS/trackLoader.js
@@ -1,5 +1,5 @@
 // Import
-import { MasterTrack } from './masterTrack.js';
+import { MasterTrack } from './masterTrack.js'; 
 import { AudioTrack } from './audioTrack.js';
 
 // Load audio files from disk
